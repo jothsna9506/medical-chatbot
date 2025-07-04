9827a05079f1afb73f3086ab0aedfcac1fbd26ac
jothsna <jothsnanaidu533@gmail.com>
2025-07-04 12:12:43 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'Generative AI project',
+    version = '0.0.0',
+    author='jothsnataehyung',
+    author_email = 'jothsnanaidu533@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
