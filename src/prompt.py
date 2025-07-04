949dfc4e3bac630b04ab02a934435d94f0400cdc
jothsna <jothsnanaidu533@gmail.com>
2025-07-04 17:47:46 +0530

    updated

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are an assistant for question_answering tasks."
+    "Use the following pieces of retrieved context to answer"
+    "the question.If you dont know the answer,say that you dont know.Use three sentences maximum and keep the"
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
